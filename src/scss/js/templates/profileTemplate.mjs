4ae36899fedf627e074faaec0b5d21a66bdd679f
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-01 23:07:57 +0200

    Made small changes - not done

--- src/scss/js/templates/profileTemplate.mjs
+++ src/scss/js/templates/profileTemplate.mjs
@@ -1,49 +1,50 @@
 export function profileContent(profileData) {
     
-    const profileContainer = document.querySelector("#profile");
-    
-    // //GET INFO FROM LOCALSTORAGE
-    // let name = localStorage.getItem("name");
-    // let email = localStorage.getItem("email");
-    // let avatar = localStorage.getItem("avatar");
-    // let banner = localStorage.getItem("banner");
-    
+    const profileContainer = document.createElement("div");
+     console.log(profileContainer)
+
     //PAGE NAME
     const pageName = document.createElement("h1");
     pageName.classList.add("text-uppercase", "m-auto");
     pageName.innerHTML = "My Profile";
+
+    const myProfile = document.createElement("div");
     
     //NAME
     const myName = document.createElement("h2");
     pageName.classList.add("text-uppercase", "m-auto");
-    pageName.innerHTML = profileData.name;
+    pageName.textContent = `Name: ${profileData.name}`;
+    console.log(profileData.name)
     
     //EMAIL
     const myEmail = document.createElement("h2");
     myEmail.classList.add("myInfo");
-    //myEmail.innerHTML = ;
+    myEmail.innerHTML =`Email: ${profileData.email}`;
 
-    //BANNER / BACKGROUND
-    const bannerImg = document.createElement("img");
-    bannerImg.classList.add("profileBackImg");
-    if (profileData.banner) {
-        bannerImg.src = profileData.banner;
-        bannerImg.alt = `Banner of: ${profileData.name}`;
-    } else {
-        return [];
-    }
+    // //BANNER / BACKGROUND
+    // const bannerImg = document.createElement("img");
+    // bannerImg.classList.add("profileBackImg");
+    // if (profileData.banner) {
+    //     bannerImg.src = profileData.banner;
+    //     bannerImg.alt = `Banner of: ${profileData.name}`;
+    // } else {
+    //     return [];
+    // }
 
-    //AVATAR
-    const avatarImg = document.createElement("img")
-    avatarImg.classList.add("avatarImg");
-    if (profileData.avatar){
-        avatarImg.src = profileData.avatar;
-        avatarImg.alt = `Avatar of: ${profileData.name}`;
-    } else {
-        img.src = "/images/moon.avif"
-    }
+    // //AVATAR
+    // const avatarImg = document.createElement("img")
+    // avatarImg.classList.add("avatarImg");
+    // if (profileData.avatar){
+    //     avatarImg.src = profileData.avatar;
+    //     avatarImg.alt = `Avatar of: ${profileData.name}`;
+    // } else {
+    //     img.src = "/images/moon.avif"
+    // }
 
-    profileContainer.append(pageName, myName, myEmail, bannerImg, avatarImg)
-    
-    return profileContainer;
+    myProfile.append(myName, myEmail);
+    profileContainer.append(pageName, myProfile);
+    console.log(profileContainer);
+
+    //return profileContainer;
+
 }
