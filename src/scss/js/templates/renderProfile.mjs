85806bc70916c9590ebe1ba9cec996126b88e484
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-02 15:26:32 +0200

    Renderprofile - done

--- src/scss/js/templates/renderProfile.mjs
+++ src/scss/js/templates/renderProfile.mjs
@@ -4,18 +4,20 @@ import { profileContent } from "./profileTemplate.mjs";
 
 export async function renderProfile(profileData) {
     try {
-        const url = new URL(location.href);
-        let name = url.searchParams.get("name");
+        // const url = new URL(location.href);
+        // let profile = url.searchParams.get("profile");
         
         const profileData = await getProfile();
         console.log(profileData);
 
         if (profileData) {
             const container = document.querySelector("#profile");
-            const profileInfo = profileContent(profileData, container);
+            const profileInfo = profileContent(profileData);
+            container.innerHTML = "";
+            console.log(profileInfo);
             container.append(profileInfo);
         } else {
-            console.log("This profile is not found:", name)
+            console.log("Profile is not found.")
         }
     } catch (error) {
         console.error("Failed to load profile:", error)
