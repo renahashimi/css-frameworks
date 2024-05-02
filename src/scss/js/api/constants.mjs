b23b28f76bee7b7668f9fa8b318cebca42e02fe1
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-02 15:29:33 +0200

    Changes URL name

--- src/scss/js/api/constants.mjs
+++ src/scss/js/api/constants.mjs
@@ -5,7 +5,7 @@ export const API_REGISTER = "/register";
 export const API_LOGIN = "/login";
 export const API_SOCIAL = "/social";
 export const API_POSTS = "/posts";
-export const API_URL = `${API_URL_BASE}${API_BASE}${API_SOCIAL}`;
+export const API_SOCIAL_URL = `${API_URL_BASE}${API_BASE}${API_SOCIAL}`;
 
 export const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzMywibmFtZSI6IlJlbmEiLCJlbWFpbCI6InJlbmFoYUBub3JvZmYubm8iLCJhdmF0YXIiOm51bGwsImJhbm5lciI6bnVsbCwiaWF0IjoxNzEzNzEwMDc4fQ.Ul3QfpEN0Q0PJXInWY1GlW3gmHrCeB37iKTxdJJRCLQ";
 
