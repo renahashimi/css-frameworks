1cad057f789421ddd45d8a9ecf6a8a1486563d0f
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-02 15:28:14 +0200

    Update index.mjs

--- src/scss/js/handlers/index.mjs
+++ src/scss/js/handlers/index.mjs
@@ -1,4 +1,5 @@
 export * from "./register.mjs";
 export * from "./login.mjs";
 export * from "./createPost.mjs";
-export * from "./editPost.mjs";
+export * from "./updatePost.mjs";
+export * from "./updateProfile.mjs";
