0b84e14fc7fb97a80965cba654fa7f17d3a5b8bf
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-06 00:30:23 +0200

    Update renderProfilePosts.mjs

--- src/scss/js/templates/renderProfilePosts.mjs
+++ src/scss/js/templates/renderProfilePosts.mjs
@@ -1,23 +1,29 @@
 import { postCard, renderPostList } from "./postTemplate.mjs";
 import * as postActions from "../api/posts/index.mjs";
+import { getProfile } from "../api/profile/get.mjs";
 
-const profileData = JSON.parse(localStorage.getItem("profile"));
+//const profileData = JSON.parse(localStorage.getItem("profile"));
 
 //RENDER MY PROFILE-POSTS
-export async function renderProfilePosts(profileData) {
+export async function renderProfilePosts() {
+    const profileData = await getProfile();
+    const profileName = profileData.name;
+    console.log(profileName, profileData)
 
-    try{
-    const posts = await postActions.getPosts();
-    
-    const postsByProfile = posts.filter(post => post.author === profileData.name);
-console.log(posts, postsByProfile, profileData)
-    const container = document.querySelector("#profilePosts");
-    container.innerHTML = "";
-    renderPostList(postsByProfile, container)
-    } catch (error) {
-        console.error("Failed to fetch your posts", error);
 
-    }
+    const response = await postActions.getPosts();
+    const allPosts = await response.json;
+    //const container = document.querySelector("#profilePosts");
+
+
+    if (profileName) {
+        return allPosts.filter(post => post.author === profileName);
+    } 
+    console.log(allPosts, profileName)
+
+    renderPostList(allPosts, container);
+
+}
         // postsByProfile.forEach(post => {
         //     const postElement = document.createElement("div");
         //     postElement.classList.add("post");
@@ -27,9 +33,8 @@ console.log(posts, postsByProfile, profileData)
         // console.log(postElement, postContent, postsData)
 
         //     postElement.appendChild(postContent);
-        //     profilePostContainer.append(postElement)
         // });
-}
+    
 
 // export function renderPostByProfileList(postDataList, parent) {
 //     postDataList.forEach(postData => {
