c049e764d7cbd8b5ad88ae23086e300985095a10
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-08 00:07:14 +0200

    Update get.mjs

--- src/scss/js/api/posts/get.mjs
+++ src/scss/js/api/posts/get.mjs
@@ -1,10 +1,11 @@
 import { authFetch } from "../authFetch.mjs";
-import { API_POSTS, API_URL } from "../constants.mjs";
+import { API_POSTS, API_SOCIAL_URL } from "../constants.mjs";
 
 // const method = "get";
+const author = "?_author=true";
 
 export async function getPosts() {
-    const getPostsUrl = `${API_URL}${API_POSTS}`;
+    const getPostsUrl = `${API_SOCIAL_URL}${API_POSTS}${author}`;
     const response = await authFetch(getPostsUrl);
     return await response.json();
 }
@@ -15,7 +16,7 @@ export async function getPost(id) {
     if (!id) {
         throw new Error("You have to have a postID to get posts");
     } 
-    const getPostUrl = `${API_URL}${API_POSTS}/${id}`;
+    const getPostUrl = `${API_SOCIAL_URL}${API_POSTS}/${id}`;
     const response = await authFetch(getPostUrl);
     return await response.json(); 
 }
