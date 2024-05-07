4c246a9fa81469db8938543dfc54d83b8ed70adb
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-07 23:53:16 +0200

    Update renderProfilePosts.mjs

--- src/scss/js/templates/renderProfilePosts.mjs
+++ src/scss/js/templates/renderProfilePosts.mjs
@@ -2,31 +2,47 @@ import { postCard, renderPostList } from "./postTemplate.mjs";
 import { getProfile } from "../api/profile/get.mjs";
 import { getPosts } from "../api/posts/index.mjs";
 import * as postActions from "../api/posts/index.mjs";
+import { load } from "../storage/index.mjs";
 
-//const profileData = JSON.parse(localStorage.getItem("profile"));
+const profileData = JSON.parse(localStorage.getItem("profile"));
 
 //RENDER MY PROFILE-POSTS
 export async function renderProfilePosts() {
     try{
-        const profileData = await getProfile();
-        const profileName = profileData.name;
+        const profile = await getProfile(profileData = load("profile").name);
+        const profileName = profile.name;
         console.log(profileName)
 
 
-        const response = await postActions.getPosts();
-        const allPosts = await response.json();
+        const storedPosts = JSON.parse(localStorage.getItem("posts")) || [];
+        console.log(storedPosts)
+        const postByProfile = storedPosts.filter(post => post.author === profileName);
+        console.log(postByProfile)
         const container = document.querySelector("#profilePosts");
+        container.innerHTML = "";
+        //renderPostList(postByProfile, container);
 
-        console.log(response.status, getPosts, allPosts)
-        if (allPosts) {
-            const postByProfile = allPosts.filter(post => post.author === profileName);
-            //renderPostList(postByProfile, container);
-            console.log(postByProfile)
-        } else {
-            console.log("Profile name not found")
-        }
+        postByProfile.forEach(postData => {
+            const postCardElement = postCard(postData);
+            container.appendChild(postCardElement)
 
-        console.log(profileName)
+        });
+
+
+        // const response = await postActions.getPosts();
+        // const allPosts = await response.json();
+        // const container = document.querySelector("#profilePosts");
+
+        // console.log(response.status, getPosts, allPosts)
+        // if (allPosts) {
+        //     const postByProfile = allPosts.filter(post => post.author === profileName);
+        //     //renderPostList(postByProfile, container);
+        //     console.log(postByProfile)
+        // } else {
+        //     console.log("Profile name not found")
+        // }
+
+        // console.log(profileName)
 
     } catch (error) {
         console.error("Failed to fetch posts", error)
