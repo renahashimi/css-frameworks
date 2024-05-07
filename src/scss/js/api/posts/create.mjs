a1c7e9bdc67afdca99aa584bb885bb6addf156a1
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-08 00:55:06 +0200

    Update create.mjs

--- src/scss/js/api/posts/create.mjs
+++ src/scss/js/api/posts/create.mjs
@@ -1,17 +1,34 @@
 import { API_POSTS, API_SOCIAL_URL } from "../constants.mjs";
 import { authFetch } from "../authFetch.mjs";
+import { load } from "../../storage/index.mjs";
 
 const method = "post";
 //const action = "/posts";
-
 export async function createPost(postData) {
     const createUrl = `${API_SOCIAL_URL}${API_POSTS}`;
-    console.log(createUrl)
-    const response = await authFetch(createUrl, {
-        method,
-        body: JSON.stringify(postData)
-    })
-    localStorage.setItem("posts", myPost)
-    window.location.href = "/profile/";
-    return await response.json();
-}  
+    try {
+        const userName = load("profile").name;
+        const postDataWithAuthor = { ...postData, author: userName };
+        console.log(postDataWithAuthor)
+        const response = await authFetch(createUrl, {
+            method,
+            body: JSON.stringify(postDataWithAuthor)
+        })
+        if (response.ok) {
+            const myPost = await response.json();
+            let posts =JSON.parse(localStorage.getItem("posts")) || [];
+            posts.push(myPost)
+            localStorage.setItem("posts", JSON.stringify(posts));
+            window.location.href = "/profile/";
+
+            return await myPost;     
+        } else {
+            throw new Error ("Failes to create post");                          
+        }
+        
+      
+    } catch (error) {
+        console.error("Failed to create post")
+        throw error;
+    }
+} 
