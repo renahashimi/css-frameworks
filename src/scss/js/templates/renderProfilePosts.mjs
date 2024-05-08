bd3f32eb7cd9dde341018616f0438edef7cdb67e
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-08 22:12:05 +0200

    Update renderProfilePosts.mjs

--- src/scss/js/templates/renderProfilePosts.mjs
+++ src/scss/js/templates/renderProfilePosts.mjs
@@ -1,82 +1,104 @@
-import { postCard, renderPostList } from "./postTemplate.mjs";
-import { getProfile } from "../api/profile/get.mjs";
-import { getPosts } from "../api/posts/index.mjs";
-import * as postActions from "../api/posts/index.mjs";
-import { load } from "../storage/index.mjs";
-import { authFetch } from "../api/authFetch.mjs";
+// import { postCard } from "./index.mjs";
+// import { getProfile } from "../api/profile/get.mjs";
+// import { load } from "../storage/index.mjs";
+// import { API_SOCIAL_URL } from "../api/constants.mjs";
+// import { authFetch } from "../api/authFetch.mjs";
+// import { getPosts } from "../api/posts/get.mjs";
 
-//const profileData = JSON.parse(localStorage.getItem("profile"));
-const author = "?_author=";
-//RENDER MY PROFILE-POSTS
-export async function renderProfilePosts() {
-   // try{
-        const profileData = await getProfile();
-        const profile = await getProfile();
-        const authorName = profile.name;
-        const getPostsUrl = `${API_SOCIAL_URL}${action}${author}${authorName}`;
-        const response = await authFetch(getPostsUrl);
-        return await response.json();
-}
-        const profileName = profileData.name;
-        console.log(profileName, profileData)
+// //const profileData = JSON.parse(localStorage.getItem("profile"));
 
+// const action = "/posts?author=";
 
-        const storedPosts = JSON.parse(localStorage.getItem("posts")) || [];
-        console.log(storedPosts)
-        const postByProfile = storedPosts.filter(post => post.name === profileName);
-        console.log(postByProfile)
-        const container = document.querySelector("#profilePosts");
-        container.innerHTML = "";
-        //renderPostList(postByProfile, container);
+// export async function getLocalstoragePosts() {
+//     try {
+//         const storedPosts = JSON.parse(localStorage.getItem("posts")) || [];
+//         return storedPosts;
+//     } catch (error) {
+//         console.error("Failed to fetch posts from localstorage:", error);
+//         return [];
+//     }
+// }
 
-        postByProfile.forEach(postData => {
-            const postCardElement = postCard(postData);
-            container.appendChild(postCardElement)
+// //RENDER MY PROFILE-POSTS
+// export async function renderProfilePosts(profile) {
+//     try{
+//         const profileData = await getProfile();
+//         const profileName = profileData.name;
+//         console.log(profileData, profileName)
 
-        });
+//        // const response = await getPosts();
+//         const allPosts = getLocalstoragePosts();
+//         console.log(posts);
+        
+//         const postByProfile = allPosts.filter((post) => post.author.name === profileName);
+//         console.log(postByProfile)
+//         const container = document.querySelector("#profilePosts");
+//         container.innerHTML = "";
 
-
-        // const response = await postActions.getPosts();
-        // const allPosts = await response.json();
-        // const container = document.querySelector("#profilePosts");
-
-        // console.log(response.status, getPosts, allPosts)
-        // if (allPosts) {
-        //     const postByProfile = allPosts.filter(post => post.author === profileName);
-        //     //renderPostList(postByProfile, container);
-        //     console.log(postByProfile)
-        // } else {
-        //     console.log("Profile name not found")
-        // }
-
-        // console.log(profileName)
-
-    } catch (error) {
-        console.error("Failed to fetch posts", error)
-    }
-}
-        // postsByProfile.forEach(post => {
-        //     const postElement = document.createElement("div");
-        //     postElement.classList.add("post");
-        //     const postContent = document.createElement("p");
-        //     postContent.textContent = "post.content";
-
-        // console.log(postElement, postContent, postsData)
-
-        //     postElement.appendChild(postContent);
-        // });
-    
-
-// export function renderPostByProfileList(postDataList, parent) {
-//     postDataList.forEach(postData => {
+//         postByProfile.forEach(postData => {
 
 //     const postElement = postCard(postData);
 //     parent.appendChild(postElement);
 //     });
 
+
+//      //   renderPostList(posts, container);
+
+
+//         // posts.forEach(postData => {
+//         //     const postCardElement = postCard(postData);
+//         //     container.appendChild(postCardElement)
+
+//         //  });
+
+
+//         // const response = await postActions.getPosts();
+//         // const allPosts = await response.json();
+//         // const container = document.querySelector("#profilePosts");
+
+//         // console.log(response.status, getPosts, allPosts)
+//         // if (allPosts) {
+//         //     //renderPostList(postByProfile, container);
+//         //     console.log(postByProfile)
+//         // } else {
+//         //     console.log("Profile name not found")
+//         // }
+
+//         // console.log(profileName)
+
+
+
+//     } catch (error) {
+//         console.error("Failed to fetch posts", error)
+//     }
 // }
 
-// export function renderPostByProfileList(postDataList, parent) {
-//     parent.append(...postDataList.map(postCard));
-// };
+// // export function renderPostListByProfile(postDataList, container) {
+// //     container.append(...postDataList.map(postData));
+// // };
+
+//         // postsByProfile.forEach(post => {
+//         //     const postElement = document.createElement("div");
+//         //     postElement.classList.add("post");
+//         //     const postContent = document.createElement("p");
+//         //     postContent.textContent = "post.content";
+
+//         // console.log(postElement, postContent, postsData)
+
+//         //     postElement.appendChild(postContent);
+//         // });
+    
+
+// // export function renderPostByProfileList(postDataList, parent) {
+// //     postDataList.forEach(postData => {
+
+// //     const postElement = postCard(postData);
+// //     parent.appendChild(postElement);
+// //     });
+
+// // }
+
+// // export function renderPostByProfileList(postDataList, parent) {
+// //     parent.append(...postDataList.map(postCard));
+// // };
 
