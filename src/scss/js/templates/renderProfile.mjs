90eaef6c0913b96accf7da3512b1b746cadfd8e8
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-08 23:46:11 +0200

    Update renderProfile.mjs

--- src/scss/js/templates/renderProfile.mjs
+++ src/scss/js/templates/renderProfile.mjs
@@ -1,29 +1,26 @@
 import { getProfile } from "../api/profile/get.mjs";
-import { postCard } from "./postTemplate.mjs";
-import { profileTemplate } from "./profileTemplate.mjs";
+import { profilePostCard, profileTemplate } from "./profileTemplate.mjs";
 
 
 export async function renderProfile() {
     try {
         const profileData = await getProfile();
-        //console.log(profileData);
+        const container = document.querySelector("#profile");
 
-       // if (profileData) {
-            const container = document.querySelector("#profile");
-            const profileContainer = profileTemplate(profileData);
-            //container.innerHTML = "";
-            //console.log(profileInfo);
-            container.append(profileContainer);
-    
-            //POSTS BY PROFILE
-            const postsContainer = document.querySelector("#profilePosts");
-            const myPosts = profileData.posts;
-            postsContainer.innerHTML = "";
+        const profileContainer = profileTemplate(profileData);
+        container.innerHTML = "";
+        //console.log(profileInfo);
+        container.append(profileContainer);
+      
+        //POSTS BY PROFILE
+        const postsContainer = document.querySelector("#profilePosts");
+        const myPosts = profileData.posts;
+        postsContainer.innerHTML = "";
 
-            myPosts.forEach(postData => {
-            const postCardElement = postCard(postData);
-            postsContainer.append(postCardElement);
-            });     
+        myPosts.forEach(postData => {
+        const postCardElement = profilePostCard(postData);
+        postsContainer.append(postCardElement);
+        });     
        // } else {
        //     console.log("Profile is not found.")
        // }
