97352df13f05eca5c36db00419361467ae94786a
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-11 22:27:02 +0200

    Update router.mjs

--- src/scss/js/router.mjs
+++ src/scss/js/router.mjs
@@ -1,12 +1,9 @@
 import * as listeners from "./handlers/index.mjs";
 import * as template from "./templates/index.mjs";
-import * as postActions from "./api/posts/index.mjs";
-
-// import * as profile from "./pages/profile/index.mjs"
+//import * as postActions from "./api/posts/index.mjs";
 
 export default function router() {
     const path = location.pathname;
-
     switch (path) {
         case "/profile/register/":
           listeners.registerFormListener();
@@ -14,23 +11,34 @@ export default function router() {
         case "/profile/login/":
           listeners.loginFormListener();
           break;
-          case "/pages/profile/":
-          // profile
+        case "/profile/":
+          template.renderProfile();
+          listeners.setLogOutListener();
+          break;
+          case "/profile/edit/":
+          listeners.setUpdateProfileListener();
+          listeners.setLogOutListener();
           break;
         case "/feed/posts/":
           template.renderAllPosts();
+          listeners.setLogOutListener();
+          listeners.searchPost();
           break;
-        // case "/feed/post/":
-        //   template.renderPostCard();
-        //   break;
-        case "/feed/post/edit/":
-          listeners.setUpdatePostListener();
+        case "/feed/post/":
+          template.renderSinglePostCard();
           break;
         case "/feed/post/create/":
           listeners.setCreatePostListener();
+          listeners.setLogOutListener();
+          break; 
+        case "/feed/post/edit/":
+          listeners.setUpdatePostListener();
+          listeners.setLogOutListener();
           break;
     }
 }
+
+
 
 // // testing function //
 // async function testTemplate() {
