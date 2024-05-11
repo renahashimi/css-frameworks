61a6a5cf4d8c8790e5e5fd5fa929630872900a1b
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-12 01:40:44 +0200

    Update filter.mjs

--- src/scss/js/handlers/filter.mjs
+++ src/scss/js/handlers/filter.mjs
@@ -1,8 +1,11 @@
+import { getPosts } from "../api/posts/get.mjs";
 import { renderPostList } from "../templates/postTemplate.mjs";
+import { renderAllPosts } from "../templates/renderPosts.mjs";
 
-export function filterPosts() {
-    const filterSelect = document.querySelector(".filterSelect");
+export async function filterPosts(seletedValue) {
+    const filterSelect = document.querySelector("#filterSelect");
     const postsContainer = document.querySelector("#posts");
+    const posts = await getPosts();
 
     filterSelect.addEventListener("change", () => {
         const seletedValue = filterSelect.value;
@@ -10,17 +13,24 @@ export function filterPosts() {
         let filteredPosts;
         switch (seletedValue) {
             case "date":
-                filteredPosts = posts.slice().sort((a, b) => new Date(a.created) - new Date(b.created));
+                filteredPosts = posts.slice().sort((a, b) => 
+                new Date(a.created) - 
+                new Date(b.created));
                 break;
             case "comments":
-                filteredPosts = posts.slice().sort((a, b) => (a.comments.length || 0) - (b.comments.length || 0));
+                filteredPosts = posts.sort((a, b) => 
+                (a.comments.length || 0) - 
+                (b.comments.length || 0));
                 break;
             case "reactions":
-                filteredPosts = posts.slice().sort((a, b) => (a.reactions.likes || 0) - (b.reactions.length || 0));
+                filteredPosts = posts.sort((a, b) => 
+                (a._count.reactions.likes || 0) - 
+                (b._count.reactions.length || 0));
                 break;
             default:
                 filteredPosts = posts;
+                console.log(typeof posts)
         }
-    renderPostList(filteredPosts, postsContainer)
+    renderAllPosts(filteredPosts, postsContainer)
     });
 }
