be0c4e144b80245fb45a5c106372b7757e4e7e71
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-12 01:15:08 +0200

    Update filter.mjs

--- src/scss/js/handlers/filter.mjs
+++ src/scss/js/handlers/filter.mjs
@@ -1,23 +1,26 @@
+import { renderPostList } from "../templates/postTemplate.mjs";
+
 export function filterPosts() {
     const filterSelect = document.querySelector(".filterSelect");
     const postsContainer = document.querySelector("#posts");
 
     filterSelect.addEventListener("change", () => {
-        const seletedValue = filterSelect.ariaValue;
+        const seletedValue = filterSelect.value;
 
         let filteredPosts;
         switch (seletedValue) {
             case "date":
-                filteredPosts = posts.sort((a, b) => new Date(a.created) - new Date(b.created));
+                filteredPosts = posts.slice().sort((a, b) => new Date(a.created) - new Date(b.created));
                 break;
             case "comments":
-                filteredPosts = posts.sort((a, b) => (a.comments.length || 0) - (b.comments.length || 0));
+                filteredPosts = posts.slice().sort((a, b) => (a.comments.length || 0) - (b.comments.length || 0));
                 break;
             case "reactions":
-                filteredPosts = posts.sort((a, b) => (a.reactions.likes || 0) - (b.reactions.length || 0));
+                filteredPosts = posts.slice().sort((a, b) => (a.reactions.likes || 0) - (b.reactions.length || 0));
                 break;
             default:
                 filteredPosts = posts;
         }
-    })
+    renderPostList(filteredPosts, postsContainer)
+    });
 }
