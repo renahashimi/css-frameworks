b7ae1340ab303bee17d143b5c6d0bd87983f748c
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-12 22:50:37 +0200

    Fixed mistakes

--- src/scss/js/api/profile/get.mjs
+++ src/scss/js/api/profile/get.mjs
@@ -3,12 +3,12 @@ import { authFetch } from "../authFetch.mjs";
 import { API_SOCIAL_URL } from "../constants.mjs";
 
 // const method = "get";
-const action = "/profiles";
-//const author = "?_author=true";
+const profile = "/profiles";
+const action = "?_author=true&_reactions=true&_comments=true";
 const posts = "?_posts=true";
 
 export async function getProfiles() {
-    const getProfilesUrl = `${API_SOCIAL_URL}${action}`;
+    const getProfilesUrl = `${API_SOCIAL_URL}${profile}${action}`;
     const response = await authFetch(getProfilesUrl);
     return await response.json();
 }
@@ -17,7 +17,7 @@ export async function getProfile(name = load("profile").name) {
     if (!name) {
         throw new Error("Requires a name");
     } 
-    const getProfileUrl = `${API_SOCIAL_URL}${action}/${name}${posts}`;
+    const getProfileUrl = `${API_SOCIAL_URL}${profile}/${name}${posts}`;
     const response = await authFetch(getProfileUrl);
     return await response.json(); 
 }
