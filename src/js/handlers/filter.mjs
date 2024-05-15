373effedfc78403a31bba666fe94796bad66b333
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-16 01:27:01 +0200

    Filter works !

--- src/js/handlers/filter.mjs
+++ src/js/handlers/filter.mjs
@@ -2,51 +2,50 @@ import { getPosts } from "../api/posts/get.mjs";
 import { renderPostList } from "../templates/postTemplate.mjs";
 import { renderAllPosts } from "./index.mjs";
 
-export async function filterPosts() {
-    const posts = await getPosts();
- 
+document.addEventListener("DOMContentLoaded", () => {
     const filterAllFeeds = document.getElementById("allFeeds");
     const filterByDate = document.getElementById("byDate");
     const filterByMedia = document.getElementById("byMedia");
-    
-    
     const postsContainer = document.getElementById("posts");
+
+    filterPosts();
+
+    filterAllFeeds.addEventListener("change", async () => {
+       filterPosts();
+    });
+    filterByDate.addEventListener("change", async () => {
+       filterPosts();
+    });
+    filterByMedia.addEventListener("change", async () => {
+       filterPosts();
+    });
+});
+export async function filterPosts() {
+    const posts = await getPosts();
+    const filterAllFeeds = document.getElementById("allFeeds");
+    const filterByDate = document.getElementById("byDate");
+    const filterByMedia = document.getElementById("byMedia");
+    const postsContainer = document.getElementById("posts");
+  
     postsContainer.innerHTML = "";
 
-   filterAllFeeds.addEventListener("change", async () => {
-        if (filterAllFeeds.checked) {
-            renderPostList(posts, postsContainer);
-            filterByDate.checked = false;
-            filterByMedia.checked = false;
-        }
-    });
-    filterByDate.addEventListener("change", () => {
-        if (filterByDate.checked) {
-           posts.sort((a, b) => new Date(a.created) - new Date(b.created));
-            console.log("DATE", posts)
-            renderPostList(posts, postsContainer);   
-            filterAllFeeds.checked = false;
-            filterByMedia.checked = false;
-        }
-    });
-    filterByMedia.addEventListener("change", () => {
-        if (filterByMedia.checked) {
-            const mediaPosts = posts.filter(post => post.media);
-            renderPostList(mediaPosts, postsContainer);
-            filterAllFeeds.checked = false;
-            filterByDate.checked = false;
-            console.log("MEDIA", mediaPosts);
-        }
-    });
-
-    if (!filterAllFeeds.checked,
-        !filterByDate.checked, 
-        !filterByMedia.checked) {
-            renderAllPosts();
-        }
-
+    if (filterAllFeeds.checked) {
+        renderPostList(posts, postsContainer);
+        filterByDate.checked = false;
+        filterByMedia.checked = false;
+    } else if (filterByDate.checked) {
+        posts.sort((a, b) => new Date(a.created) - new Date(b.created));
+        console.log("DATE", posts)
+        renderPostList(posts, postsContainer);   
+        filterAllFeeds.checked = false;
+        filterByMedia.checked = false;
+    } else if (filterByMedia.checked) {
+        const mediaPosts = posts.filter(post => post.media);
+        renderPostList(mediaPosts, postsContainer);
+        filterAllFeeds.checked = false;
+        filterByDate.checked = false;
+        console.log("MEDIA", mediaPosts);
+    } else {
+        renderAllPosts();
+    }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    filterPosts();
-});
