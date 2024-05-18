02482237e8757c5a111ca64cfc91ef5603687842
Rena Hashimi <renhas54143@stud.noroff.no>
2024-05-18 22:59:46 +0200

    Deleted by other

--- src/scss/js/api/posts/index.mjs
+++
@@ -1,7 +0,0 @@
-export * from "./create.mjs";
-export * from "./get.mjs";
-export * from "./update.mjs";
-export * from "./delete.mjs";
-export * from "./comment.mjs";
-export * from "./deleteComment.mjs";
-export * from "./reaction.mjs"
--- src/scss/js/api/profile/index.mjs
+++
@@ -1,2 +0,0 @@
-export * from "./get.mjs";
-export * from "./update.mjs";
